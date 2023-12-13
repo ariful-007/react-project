37a3ef87f696053ca9a84fe83c4a7ff938ed472e
ariful007 <arifulislamwd007@gmail.com>
2023-12-13 20:15:40 +0600

    home update

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,11 @@ import './App.css'
 import {Routes,Route} from "react-router-dom"
 import Hedar from './componets/Hedar'
 import Footer from './componets/Footer'
-import Home from './Pages/Home'
 import About from './Pages/About'
 import Blogo from './Pages/Blogo'
 import Contacet from './Pages/Contacet'
-import Banner from './componets/Banner'
+import Home from './Pages/Home'
 import Servis from './Pages/Servis'
-import Testmonial from './componets/Testmonial'
-import Users from './componets/Users'
 
 
 function App() {
@@ -17,16 +14,13 @@ function App() {
   return (
     <>
       <Hedar></Hedar>
-      <Banner></Banner>
       <Routes>
-        <Route path="/" element={<Home></Home>} /> 
+        <Route path="/home" element={<Home></Home>} /> 
         <Route path="/about" element={<About></About>} /> 
         <Route path="/services" element={<Servis></Servis>} /> 
         <Route path="/blog" element={<Blogo></Blogo>} />
         <Route path="/contact" element={<Contacet></Contacet>} /> 
       </Routes>
-      <Users></Users>
-      <Testmonial></Testmonial>
       <Footer></Footer>
     </>
   )
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,21 @@
-
+import Aboute from "../componets/Aboute";
+import Banner from "../componets/Banner"
+import Projects from "../componets/Projects";
+import Services from "../componets/Services";
+import Users from "../componets/Users";
+import Testmonial from "../componets/Testmonial";
+import Contact from "../componets/Contact";
 
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
+      <Banner></Banner>
+      <Aboute></Aboute>
+      <Services></Services>
+      <Projects></Projects>
+      <Users></Users>
+      <Testmonial></Testmonial>
+      <Contact></Contact>
     </div>
   );
 };
